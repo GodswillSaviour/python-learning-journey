1d671d5987942d2a04ea6ac574d183b3c5e8f5ab
GodswillSaviour <godswillsaviour89@gmail.com>
2026-09-01 12:15:05 +0100

    Learn Python variables and value storage

---
+++ 02-variables/variables.py
@@ -0,0 +1,9 @@
+# Variables store values that can be accessed and reused in a program.
+name = "Godswill"
+age = 24
+country = "Nigeria"
+
+# The print() function displays the specified values in the console.
+print(name)
+print(age)
+print(country)
