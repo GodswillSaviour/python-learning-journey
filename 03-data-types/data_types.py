97792de0865b1c451016abc8ec7198b02181e3bc
GodswillSaviour <godswillsaviour89@gmail.com>
2026-09-01 12:27:28 +0100

    Explore Python fundamental data types

---
+++ 03-data-types/data_types.py
@@ -0,0 +1,19 @@
+# A data type defines the kind of value that a variable stores.
+
+# The str data type stores text.
+name = "Godswill"
+
+# The int data type stores whole numbers.
+age = 24
+
+# The float data type stores numbers with decimal values.
+height = 6.3
+
+# The bool data type stores either True or False.
+is_student = True
+
+# The type() function returns the data type of the specified value.
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
