9bdc38c5a23c8379585681d03bb93af2163eed11
GodswillSaviour <godswillsaviour89@gmail.com>
2026-09-01 15:10:49 +0100

    Learn while loop repetition

---
+++ 07-loops/while_loop.py
@@ -0,0 +1,11 @@
+# Store the starting number that the while loop will check and update.
+number = 1
+
+# Continue repeating the loop while number is less than or equal to 5.
+while number <= 5:
+
+    # Display the current value of number.
+    print(number)
+
+    # Increase number by 1 after each iteration.
+    number += 1
