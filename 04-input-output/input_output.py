137d4ac60c26402030b585180169f6b2b6cbbb18
GodswillSaviour <godswillsaviour89@gmail.com>
2026-09-01 12:40:32 +0100

    Practice Python input and output

---
+++ 04-input-output/input_output.py
@@ -0,0 +1,7 @@
+# The input() function receives text entered by the user and returns it as a string.
+name = input("Enter your name: ")
+favourite = input("Enter your favourite programming language: ")
+
+# The print() function displays the specified values in the console.
+print("Hello", name)
+print("Your favourite programming language is", favourite)
