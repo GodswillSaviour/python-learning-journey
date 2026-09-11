07bffe3fb6a4579025d26960c04e38b38815d50c
GodswillSaviour <godswillsaviour89@gmail.com>
2026-09-11 09:59:40 +0100

    Create first reusable Python function

---
+++ 08-functions/hello_function.py
@@ -0,0 +1,9 @@
+# Define a function named greet.
+def greet():
+
+    # Display a welcome message.
+    print("Hello, welcome to Python!")
+
+
+# Call the greet function to run its instructions.
+greet()
