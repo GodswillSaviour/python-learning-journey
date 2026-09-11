847ad98f27f9dfe00e5bb591bfc769aee98b8bc0
GodswillSaviour <godswillsaviour89@gmail.com>
2026-09-11 10:16:39 +0100

    Practice functions with parameters

---
+++ 08-functions/greet_person.py
@@ -0,0 +1,9 @@
+# Define a function named greet that accepts a person's name.
+def greet(name):
+
+    # Display a greeting using the name passed to the function.
+    print("Hello", name + "!")
+
+
+# Call the greet function and pass "Godswill" as the argument.
+greet("Godswill")
